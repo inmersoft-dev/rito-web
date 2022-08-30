7c1240c72bc839e2e8b7b2c09504ecd5509b800b
Sito8943 <sito8943@gmail.com>
2022-08-29 20:41:52 -0400

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+// @mui components
+import { Link } from "@mui/material";
+
+// sito components
+import SitoContainer from "sito-container";
+import SitoImage from "sito-image";
+
+// utils
+import { scrollTo } from "../utils/functions";
+
+// contexts
+import { useLanguage } from "../context/LanguageProvider";
 
 const Navbar = () => {
-  return <div></div>;
+  const { languageState } = useLanguage();
+
+  return (
+    <SitoContainer>
+      <SitoImage sx={{ width: "100px", height: "40px" }} />
+      <SitoContainer>
+        {Object.values(languageState.texts.Links).map((item) => (
+          <Link href={item.to} key={item.To}>
+            {item.label}
+          </Link>
+        ))}
+      </SitoContainer>
+    </SitoContainer>
+  );
 };
 
 export default Navbar;
