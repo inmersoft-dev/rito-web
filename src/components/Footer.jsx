07f1426761315659472e4dfa8da68cf02a338782
Sito8943 <sito8943@gmail.com>
2022-09-01 11:32:31 -0400

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+// sito components
+import SitoContainer from "sito-container";
+
+// @mui components
+import { Box, Link } from "@mui/material";
+
+// @mui icons
+import { Facebook, Twitter, LinkedIn } from "@mui/icons-material";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <Box sx={{ display: "flex", width: "100%", height: "60px" }}>
+      <SitoContainer>
+        <Link href="https://facebook.com">
+          <Facebook />
+        </Link>
+        <Link href="https://twitter.com">
+          <Twitter />
+        </Link>
+        <Link href="https://linkedin.com">
+          <LinkedIn />
+        </Link>
+      </SitoContainer>
+    </Box>
+  );
 };
 
 export default Footer;
